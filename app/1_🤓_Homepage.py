077afcf3dc513e5f533d5aaf0c4b018ed2abea84
NilodioBrite <danilo.diaz@britecore.com>
2024-04-17 19:35:47 -0400

    Mock front end

--- app/1_🤓_Homepage.py
+++ app/1_🤓_Homepage.py
@@ -1,9 +1,14 @@
 import os
-
+import pandas as pd
 import streamlit as st
-from streamlit_extras import add_vertical_space as add_vertical_space
+from streamlit_extras import add_vertical_space as avs
+import plotly.express as px
+import plotly.graph_objects as go
+from annotated_text import annotated_text
 from streamlit_extras.badges import badge
-from utils import update_st_state
+from utils import update_st_state, create_annotated_text, create_star_graph
+
+from jobsparser import ParseJobDesc, ParseResume, TextUtils
 
 # Set page configuration
 st.set_page_config(
@@ -38,7 +43,7 @@ with st.sidebar:
 
 # Divider and vertical space
 st.divider()
-add_vertical_space(1)
+avs.add_vertical_space(1)
 
 with st.container():
     resume_col, job_description_col = st.columns(2)
@@ -85,3 +90,170 @@ with st.container():
         else:
             update_st_state("job_description_uploaded", "Pending")
             update_st_state("job_description_path", "")
+
+with st.spinner("Please wait..."):
+    if (
+        uploaded_resume is not None
+        and st.session_state["job_description_uploaded"] == "Uploaded"
+        and uploaded_job_description is not None
+        and st.session_state["job_description_uploaded"] == "Uploaded"
+    ):
+        resume_processor = ParseResume(
+            TextUtils().read_single_pdf(st.session_state["resume_path"])
+        )
+        job_description_processor = ParseJobDesc(
+            TextUtils().read_single_pdf(st.session_state["job_description_path"])
+        )
+
+        selected_file = resume_processor.get_JSON()
+        selected_jd = job_description_processor.get_JSON()
+
+        with st.container():
+            resumeCol, jobDescriptionCol = st.columns(2)
+            with resumeCol:
+                with st.expander("Parsed Resume Data"):
+                    st.caption("This text is parsed from your resume.")
+                    avs.add_vertical_space(3)
+                    st.write(selected_file["clean_data"])
+
+            with jobDescriptionCol:
+                with st.expander("Parsed Job Description"):
+                    st.caption("parsing from PDF but it'll be from txt or copy paste.")
+                    avs.add_vertical_space(3)
+                    st.write(selected_jd["clean_data"])
+
+        with st.container():
+            resumeCol, jobDescriptionCol = st.columns(2)
+            with resumeCol:
+                with st.expander("Extracted Keywords"):
+                    st.write(
+                        "Now let's take a look at the extracted keywords from the resume."
+                    )
+                    annotated_text(
+                        create_annotated_text(
+                            selected_file["clean_data"],
+                            selected_file["extracted_keywords"],
+                            "KW",
+                            "#CCFFFF",
+                        )
+                    )
+            with jobDescriptionCol:
+                with st.expander("Extracted Keywords"):
+                    st.write(
+                        "Now let's take a look at the extracted keywords from the job description."
+                    )
+                    annotated_text(
+                        create_annotated_text(
+                            selected_jd["clean_data"],
+                            selected_jd["extracted_keywords"],
+                            "KW",
+                            "#0B666A",
+                        )
+                    )
+
+        with st.container():
+            resumeCol, jobDescriptionCol = st.columns(2)
+            with resumeCol:
+                with st.expander("Extracted Entities"):
+                    st.write(
+                        "Now let's take a look at the extracted entities from the resume."
+                    )
+                    create_star_graph(selected_file["keyterms"], "Entities from Resume")
+            with jobDescriptionCol:
+                with st.expander("Extracted Entities"):
+                    st.write(
+                        "Now let's take a look at the extracted entities from the job description."
+                    )
+                    create_star_graph(
+                        selected_jd["keyterms"], "Entities from Job Description"
+                    )
+
+        with st.container():
+            resumeCol, jobDescriptionCol = st.columns(2)
+            with resumeCol:
+                with st.expander("Keywords & Values"):
+                    df1 = pd.DataFrame(
+                        selected_file["keyterms"], columns=["keyword", "value"]
+                    )
+                    keyword_dict = {}
+                    for keyword, value in selected_file["keyterms"]:
+                        keyword_dict[keyword] = value * 100
+
+                    fig = go.Figure(
+                        data=[
+                            go.Table(
+                                header=dict(
+                                    values=["Keyword", "Value"],
+                                    font=dict(size=12, color="white"),
+                                    fill_color="#1d2078",
+                                ),
+                                cells=dict(
+                                    values=[
+                                        list(keyword_dict.keys()),
+                                        list(keyword_dict.values()),
+                                    ],
+                                    line_color="darkslategray",
+                                    fill_color="#6DA9E4",
+                                ),
+                            )
+                        ]
+                    )
+                    st.plotly_chart(fig, use_container_width=True)
+            with jobDescriptionCol:
+                with st.expander("Keywords & Values"):
+                    df2 = pd.DataFrame(
+                        selected_jd["keyterms"], columns=["keyword", "value"]
+                    )
+
+                    # Create the dictionary
+                    keyword_dict = {}
+                    for keyword, value in selected_jd["keyterms"]:
+                        keyword_dict[keyword] = value * 100
+
+                    fig = go.Figure(
+                        data=[
+                            go.Table(
+                                header=dict(
+                                    values=["Keyword", "Value"],
+                                    font=dict(size=12, color="white"),
+                                    fill_color="#1d2078",
+                                ),
+                                cells=dict(
+                                    values=[
+                                        list(keyword_dict.keys()),
+                                        list(keyword_dict.values()),
+                                    ],
+                                    line_color="darkslategray",
+                                    fill_color="#6DA9E4",
+                                ),
+                            )
+                        ]
+                    )
+                    st.plotly_chart(fig, use_container_width=True)
+
+        with st.container():
+            resumeCol, jobDescriptionCol = st.columns(2)
+            with resumeCol:
+                with st.expander("Key Topics"):
+                    fig = px.treemap(
+                        df1,
+                        path=["keyword"],
+                        values="value",
+                        color_continuous_scale="Rainbow",
+                        title="Key Terms/Topics Extracted from your Resume",
+                    )
+                    st.plotly_chart(fig, use_container_width=True)
+
+            with jobDescriptionCol:
+                with st.expander("Key Topics"):
+                    fig = px.treemap(
+                        df2,
+                        path=["keyword"],
+                        values="value",
+                        color_continuous_scale="Rainbow",
+                        title="Key Terms/Topics Extracted from Job Description",
+                    )
+                    st.plotly_chart(fig, use_container_width=True)
+
+st.divider()
+st.markdown("[:arrow_up: Back to Top](#resume-matcher)")
--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,127 @@
 import streamlit as st
+import nltk
+import networkx as nx
+import plotly.graph_objects as go
 
 
 def update_st_state(key, val):
     st.session_state[key] = val
+
+
+def create_annotated_text(
+    input_string: str, word_list: list, annotation: str, color_code: str
+):
+    """
+    Annotates the input string based on the word list provided.
+    """
+    tokens = nltk.word_tokenize(input_string)
+
+    word_set = set(word_list)
+
+    ret_annotated_text = []
+
+    for token in tokens:
+        if token in word_set:
+            ret_annotated_text.append((token, annotation, color_code))
+        else:
+            ret_annotated_text.append(token)
+
+    return ret_annotated_text
+
+
+def create_star_graph(nodes_and_weights, title):
+    """
+    Create a star-shaped graph visualization.
+
+    Args:
+        nodes_and_weights (list): List of tuples containing nodes and their weights.
+        title (str): Title for the graph.
+
+    Returns:
+        None
+    """
+    # Create an empty graph
+    graph = nx.Graph()
+
+    # Add the central node
+    central_node = "resume"
+    graph.add_node(central_node)
+
+    # Add nodes and edges with weights to the graph
+    for node, weight in nodes_and_weights:
+        graph.add_node(node)
+        graph.add_edge(central_node, node, weight=weight * 100)
+
+    # Get position layout for nodes
+    pos = nx.spring_layout(graph)
+
+    # Create edge trace
+    edge_x = []
+    edge_y = []
+    for edge in graph.edges():
+        x0, y0 = pos[edge[0]]
+        x1, y1 = pos[edge[1]]
+        edge_x.extend([x0, x1, None])
+        edge_y.extend([y0, y1, None])
+
+    edge_trace = go.Scatter(
+        x=edge_x,
+        y=edge_y,
+        line=dict(width=0.5, color="#888"),
+        hoverinfo="none",
+        mode="lines",
+    )
+
+    # Create node trace
+    node_x = []
+    node_y = []
+    for node in graph.nodes():
+        x, y = pos[node]
+        node_x.append(x)
+        node_y.append(y)
+
+    node_trace = go.Scatter(
+        x=node_x,
+        y=node_y,
+        mode="markers",
+        hoverinfo="text",
+        marker=dict(
+            showscale=True,
+            colorscale="Rainbow",
+            reversescale=True,
+            color=[],
+            size=10,
+            colorbar=dict(
+                thickness=15,
+                title="Node Connections",
+                xanchor="left",
+                titleside="right",
+            ),
+            line_width=2,
+        ),
+    )
+
+    node_adjacencies = []
+    node_text = []
+    for node in graph.nodes():
+        adjacencies = list(graph.adj[node])
+        node_adjacencies.append(len(adjacencies))
+        node_text.append(f"{node}<br># of connections: {len(adjacencies)}")
+
+    node_trace.marker.color = node_adjacencies
+    node_trace.text = node_text
+
+    figure = go.Figure(
+        data=[edge_trace, node_trace],
+        layout=go.Layout(
+            title=title,
+            titlefont=dict(size=16),
+            showlegend=False,
+            hovermode="closest",
+            margin=dict(b=20, l=5, r=5, t=40),
+            xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+            yaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+        ),
+    )
+
+    st.plotly_chart(figure, use_container_width=True)
